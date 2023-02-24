fbaa17277d268d1b4f99793822e352e1ac3b69e4
matheus-g-a <matheus99@gmail.com>
2023-02-23 21:54:45 -0800

    final commit

---
+++ PyBank/main.py
@@ -0,0 +1,64 @@
+import csv
+import os
+from pathlib import Path
+
+# getting the csv path using os library
+budget_path = os.path.join("PyBank", "Resources", "budget_data.csv")
+# create 2 lists to receive the data
+date = []
+budget = []
+
+# open and read csv
+with open(budget_path, 'r') as budget_file: 
+    csv_reader = csv.reader(budget_file)
+    csv_header = next(csv_reader)
+#appending the data to the lists
+
+    for row in csv_reader:
+        date.append(row[0])
+        budget.append(int(row[1]))
+# calculate the number of months, net changes and increses and decreases on the dataset
+total_months = len(date)
+total_net = 0
+for row in budget:
+    total_net += row
+print(total_net)
+total_changes = []
+
+for row in range(len(budget)-1):
+    total_changes.append(budget[row+1]-budget[row])
+
+avg_changes = round(sum(total_changes) / len(total_changes),2)
+
+
+max_increase =  max(total_changes)
+
+month_max = date[total_changes.index(max_increase)+1]
+min_increase =  min(total_changes)
+month_min = date[total_changes.index(min_increase)+1]
+
+# print the analyses with the f function
+print(f"""
+Financial Analysis
+----------------------------
+Total Months: {total_months}
+Total: ${total_net}
+Average Change: ${avg_changes}
+Greatest Increase in Profits: {month_max} ${max_increase}
+Greatest Decrease in Profits: {month_min} ${min_increase}
+"""
+)
+
+output_analysis = os.path.join("PyBank", "analysis", "results.txt")
+
+with open(output_analysis,'w') as analysis:
+
+    analysis.write(f"""
+Financial Analysis
+----------------------------
+Total Months: {total_months}
+Total: ${total_net}
+Average Change: ${avg_changes}
+Greatest Increase in Profits: {month_max} ${max_increase}
+Greatest Decrease in Profits: {month_min} ${min_increase}
+""")
---
+++ PyPoll/main.py
@@ -0,0 +1,72 @@
+import csv
+import os
+from pathlib import Path
+
+# getting the csv path using os library
+election_path = os.path.join("PyPoll", "Resources", "election_data.csv")
+vote_id = []
+county = []
+candidates = []
+votes_count = 0
+
+votes_charles = 0
+votes_diana = 0
+votes_raymon = 0
+
+# open and read csv
+with open(election_path, 'r') as election_file: 
+    csv_reader = csv.reader(election_file, delimiter=",")
+    csv_header = next(csv_reader)
+    # appending the data to the lists
+    for row in csv_reader:
+        vote_id.append(row[0])
+        county.append(row[1])
+        candidates.append(row[2])
+    # count the total votes
+        votes_count += 1
+        # counting the votes for each candidate
+        if row[2] =="Charles Casper Stockham":
+            votes_charles += 1
+        elif row[2] == "Diana DeGette":
+            votes_diana += 1
+        elif row[2] == "Raymon Anthony Doane":
+            votes_raymon +=1
+  # calculate the total votes for each candidate           
+perc_charles = round((votes_charles/votes_count * 100),3)
+perc_diana = round((votes_diana/votes_count * 100),3)
+perc_raymon = round((votes_raymon/votes_count * 100),3)
+#create a dictonary with each candidate and their votes to calculate the winner
+winner_dict = {"Charles Casper Stockham": votes_charles, "Diana DeGette": votes_diana, "Raymon Anthony Doane": votes_raymon}
+#get the winner from the dictionary using .get metod
+winner = max(winner_dict, key = winner_dict.get)
+#print the result using f string funciotn
+print(f"""
+Election Results
+-------------------------
+Total Votes: {votes_count}
+-------------------------
+Charles Casper Stockham: {perc_charles}% {votes_charles}
+Diana DeGette: {perc_diana}% {votes_diana}
+Raymon Anthony Doane: {perc_raymon}% {votes_raymon}
+-------------------------
+Winner: {winner}
+-------------------------
+""")
+
+    
+output_analysis = os.path.join("PyPull", "analysis", "results.txt")
+
+with open(output_analysis,'w') as analysis:
+
+    analysis.write(f"""
+Election Results
+-------------------------
+Total Votes: {votes_count}
+-------------------------
+Charles Casper Stockham: {perc_charles}% {votes_charles}
+Diana DeGette: {perc_diana}% {votes_diana}
+Raymon Anthony Doane: {perc_raymon}% {votes_raymon}
+-------------------------
+Winner: {winner}
+-------------------------
+""")
